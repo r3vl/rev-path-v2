1897e233499cf9aa167e0359c76d98a1eff376f5
Tirtha Sarker <tsark99@gmail.com>
2023-02-02 12:59:19 +0530

    aggregator helper contract

---
+++ contracts/helper/RevenueAggregatorV2.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: SPWPL
+
+pragma solidity 0.8.15;
+
+contract RevenueAggregatorV2 {
+    /** @notice Emits when a withdrawal takes place
+     * @param path The address of the revenue path
+     * @param status Whether the withdrawal suceeded
+     */
+    event WithdrawStatus(address indexed path, bool indexed status, bytes result);
+    error ZeroAddressProvided();
+
+    /** @notice Batch withdrawal request for tokens across revenue paths
+     * @param paths List of revenue paths
+     * @param targetWallet The wallet for which the withdrawal is being made
+     */
+    function withdrawPathToken(address[] calldata paths, address targetWallet, address tokenAddress) external {
+        uint256 pathLength = paths.length;
+
+        for (uint256 i = 0; i < pathLength; ) {
+            if(paths[i] == address(0)){
+                revert ZeroAddressProvided();
+            }
+            (bool status, bytes memory result) = address(paths[i]).call(
+                abi.encodeWithSignature("release(address,address)",tokenAddress, targetWallet)
+            );
+            
+            emit WithdrawStatus(paths[i], status, result);
+            unchecked{i++;}
+        }
+    }
+
+   
+}
